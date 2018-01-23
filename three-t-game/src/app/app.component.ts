d591ee6cf742d99164df40a7b2f2b6df51d9cf3b
Clarence <clarencer89@gmail.com>
2018-01-23 17:43:26 +0200

    Remove mvc app, will create webapi

--- three-t-game/src/app/app.component.ts
+++ three-t-game/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Assets } from './core/assets';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  Asset = Assets;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
---
+++ three-t-game/src/app/core/assets.ts
@@ -0,0 +1,4 @@
+export class Assets {
+    static ResetIcon = './assets/reset-game-icon.svg';
+    static NewIcon = './assets/new-game-icon.svg';
+}
